7b27341ee8e4bf403c4ae25a9902c2cf531791f5
Chiara Zuccaro <chiara-zuc@libero.it>
2022-04-06 11:06:52 +0200

    user can add a city

--- js/weatherapp.js
+++ js/weatherapp.js
@@ -1,19 +1,19 @@
-import { addForm, cityListHome } from "./addingCity.js";
+import { q, cityListComplete } from "./addingCity.js";
 import { addOption, getApi, createCard, selectCity, removeCard } from "./function.js";
 
 
 // Loading cities & creating card & filter
-for(let i = 0; i < cityListHome.length; i++) {
-    addOption(cityListHome[i].city);
-    getApi(cityListHome[i].city).then((data) => {
-        createCard(data, cityListHome[i].icon);
+for(let i = 0; i < cityListComplete.length; i++) {
+    addOption(cityListComplete[i].city);
+    getApi(cityListComplete[i].city).then((data) => {
+        createCard(data, cityListComplete[i].icon);
     }).then(() => {        
         const cardEls = document.querySelectorAll(".card");
         const selector = document.querySelector("#city-selector");
         
         //                        FILTER       CITY
         selector.addEventListener("click", () => {    
-            if(selector.selectedIndex > 0 && selector.selectedIndex <= cityListHome.length) {            
+            if(selector.selectedIndex > 0 && selector.selectedIndex <= cityListComplete.length) {            
                 cardEls.forEach(element => {
                     if(element.outerHTML.toLowerCase().split("").join("").includes(selector.value)) {
                         selectCity(element);
@@ -21,8 +21,8 @@ for(let i = 0; i < cityListHome.length; i++) {
                 });
             } else {        
                 removeCard();
-                getApi(cityListHome[i].city).then((data) => {
-                    createCard(data, cityListHome[i].icon);
+                getApi(cityListComplete[i].city).then((data) => {
+                    createCard(data, cityListComplete[i].icon);
                 });
             }
         });
@@ -33,8 +33,23 @@ for(let i = 0; i < cityListHome.length; i++) {
 
 
 // form
-document.getElementById("add").addEventListener("click", () => {
-    addForm();
-    document.getElementById("add").classList.remove("add-btn");
-    document.getElementById("add").classList.add("hidden");
+document.getElementById("confirm").addEventListener("click", () => {
+    event.preventDefault();
+    const inputCity = q("#city");
+    const inputImg = q("#img");
+
+    cityListComplete.push({
+        city: inputCity.value,
+        icon: inputImg.value,
+        saves: ""
+    });
+
+    console.log(cityListComplete);
+        
+    for(let i = 0; i < cityListComplete.length; i++) {
+        localStorage.setItem("cities", JSON.stringify(cityListComplete[i]));
+        console.log("storage");
+    }
+    inputCity.value = "";
+    inputImg.value = "";
 });
